b3ad7087fc51f1b97da693f86f4956e06991e942
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-12 09:35:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let app=document.getElementById("app");
 let ans=true;
 document.getElementById("swap").addEventListener("click",(e)=>{
 	if(ans){
-		app.classList.add("night");
+		app.classList.add("button_day");
 		app.classList.remove("button_night");
 		ans=false;
 	}
     else{
 		app.classList.add("button_night")
-		app.classList.remove("night");
+		app.classList.remove("button_day");
 		ans=true;
 	}
 	   
