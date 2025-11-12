5a925151852067583256ce9d7091cd9f3e5403d4
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-12 09:56:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-let app=document.getElementById("app");
-let button=document.getElementById("swap")
-let ans=true;
-document.getElementById("swap").addEventListener("click",(e)=>{
-	if(ans){
-		app.classList.add("night");
-		button.classList.remove("button_day");
-		button.classList.remove("button_night");
-		ans=false;
-	}
-    else{
-		button.classList.add("button_night")
-		button.classList.add("button_day");
-		app.classList.remove("night")
-		ans=true;
-	}
-	   
-})
+let app = document.getElementById("app");
+let button = document.getElementById("swap");
+let ans = true;
+
+button.addEventListener("click", () => {
+  if (ans) {
+    app.classList.add("night");
+    button.classList.remove("button_day");
+    button.classList.add("button_night");
+    ans = false;
+  } else {
+    app.classList.remove("night");
+    button.classList.remove("button_night");
+    button.classList.add("button_day");
+    ans = true;
+  }
+});
 
